640976b7d0af13b49a574877c77308c4ee1a1555
KostaUrumov <koceurumov@gmail.com>
2023-08-15 17:42:07 +0000

    area small update

---
+++ OnlineCarMarket/Areas/Administrator/Controllers/BaseController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineCarMarket.Areas.Administrator.Controllers
+{
+    [Area("Administrator")]
+    [Route("/Administrator/[controller]/[Action]/{id?}")]
+    [Authorize(Policy = "AdminsOnly")]
+    public class BaseController : Controller
+    {
+        
+    }
+}
---
+++ OnlineCarMarket/Areas/Administrator/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineCarMarket.Areas.Administrator.Controllers
+{
+    public class HomeController : BaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
